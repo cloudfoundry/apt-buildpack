69a8bdb924cf12b8b6435ad15a76c8f7bc5391ba
Sam Coward <scoward@pivotal.io>
2017-10-31 16:34:46 -0400

    Fix brats test

--- src/apt/brats/brats_test.go
+++ src/apt/brats/brats_test.go
@@ -1,6 +1,8 @@
 package brats_test
 
 import (
+	"html/template"
+	"os"
 	"path/filepath"
 
 	"github.com/cloudfoundry/libbuildpack/cutlass"
@@ -10,12 +12,40 @@ import (
 )
 
 var _ = Describe("Apt supply buildpack", func() {
-	var app *cutlass.App
-	AfterEach(func() { app = DestroyApp(app) })
+	var (
+		app    *cutlass.App
+		repo   *cutlass.App
+		appDir string
+		err    error
+	)
+	AfterEach(func() {
+		app = DestroyApp(app)
+		repo = DestroyApp(repo)
+		Expect(os.RemoveAll(appDir)).To(Succeed())
+	})
 
 	Context("Unbuilt buildpack (eg github)", func() {
 		BeforeEach(func() {
-			app = cutlass.New(filepath.Join(bpDir, "fixtures", "simple"))
+			repo = cutlass.New(filepath.Join(bpDir, "fixtures", "repo"))
+			repo.Buildpacks = []string{"staticfile_buildpack"}
+			PushApp(repo)
+
+			appDir, err = cutlass.CopyFixture(filepath.Join(bpDir, "fixtures", "simple"))
+			Expect(err).NotTo(HaveOccurred())
+
+			repoBaseUrl, err := repo.GetUrl("/")
+			Expect(err).NotTo(HaveOccurred())
+
+			aptYamlTemplate := template.New("apt.yml")
+			_, err = aptYamlTemplate.ParseFiles(filepath.Join(bpDir, "fixtures", "simple", "apt.yml"))
+			Expect(err).ToNot(HaveOccurred())
+
+			aptYaml, err := os.Create(filepath.Join(appDir, "apt.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(aptYamlTemplate.Execute(aptYaml, map[string]string{"repoBaseURL": repoBaseUrl})).To(Succeed())
+			Expect(aptYaml.Close()).To(Succeed())
+
+			app = cutlass.New(appDir)
 			app.Buildpacks = []string{buildpacks.Unbuilt, "binary_buildpack"}
 		})
 
@@ -24,7 +54,7 @@ var _ = Describe("Apt supply buildpack", func() {
 			Expect(app.Stdout.String()).To(ContainSubstring("-----> Download go 1.9"))
 
 			Expect(app.Stdout.String()).To(ContainSubstring("Installing apt packages"))
-			Expect(app.GetBody("/")).To(ContainSubstring("Ascii: ASCII 6/4 is decimal 100, hex 64"))
+			Expect(app.GetBody("/bosh")).To(ContainSubstring("BOSH: version 2"))
 		})
 	})
 })
