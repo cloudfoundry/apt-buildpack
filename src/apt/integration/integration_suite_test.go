44a12b364a8a8aeb425aedabc664fce255224596
Zander Mackie <amackie@pivotal.io>
2019-11-08 11:18:47 -0500

    Address InternetTraffic api changes
    [#169633880]

--- src/apt/integration/integration_suite_test.go
+++ src/apt/integration/integration_suite_test.go
@@ -122,7 +122,6 @@ func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
 			defer os.Remove(bpFile)
 
 			traffic, built, _, err := cutlass.InternetTraffic(
-				bpDir,
 				filepath.Join("fixtures", fixtureName),
 				bpFile,
 				[]string{"HTTP_PROXY=" + proxy.URL, "HTTPS_PROXY=" + proxy.URL},
@@ -153,7 +152,6 @@ func AssertNoInternetTraffic(fixtureName string) {
 		defer os.Remove(bpFile)
 
 		traffic, built, _, err := cutlass.InternetTraffic(
-			bpDir,
 			filepath.Join("fixtures", fixtureName),
 			bpFile,
 			[]string{},
