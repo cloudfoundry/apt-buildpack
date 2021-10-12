708247f9b108e6f404b4a62b1545928927092b02
Cloud Foundry Buildpacks Team Robot <cf-buildpacks-eng@pivotal.io>
2021-10-12 15:32:27 +0000

    Updating github-config

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,21 +7,68 @@ set -o pipefail
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 readonly ROOTDIR
 
+# shellcheck source=SCRIPTDIR/.util/print.sh
+source "${ROOTDIR}/scripts/.util/print.sh"
+
 # shellcheck source=SCRIPTDIR/.util/tools.sh
 source "${ROOTDIR}/scripts/.util/tools.sh"
 
+function usage() {
+  cat <<-USAGE
+integration.sh --github-token <token> [OPTIONS]
+Runs the integration tests.
+OPTIONS
+  --help                  -h  prints the command usage
+  --github-token <token>      GitHub token to use when making API requests
+  --platform <cf|docker>      Switchblade platform to execute the tests against
+USAGE
+}
+
 function main() {
-  local src stack harness
+  local src stack platform token
   src="$(find "${ROOTDIR}/src" -mindepth 1 -maxdepth 1 -type d )"
-  stack="$(jq -r -S .stack "${ROOTDIR}/config.json")"
-  harness="$(jq -r -S .integration.harness "${ROOTDIR}/config.json")"
+  stack="${CF_STACK:-$(jq -r -S .stack "${ROOTDIR}/config.json")}"
+  platform="cf"
+
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --platform)
+        platform="${2}"
+        shift 2
+        ;;
+
+      --github-token)
+        token="${2}"
+        shift 2
+        ;;
+
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        util::print::error "unknown argument \"${1}\""
+    esac
+  done
+
+  if [[ "${platform}" == "docker" ]]; then
+    if [[ "$(jq -r -S .integration.harness "${ROOTDIR}/config.json")" != "switchblade" ]]; then
+      util::print::warn "NOTICE: This integration suite does not support Docker."
+    fi
+  fi
 
   IFS=$'\n' read -r -d '' -a matrix < <(
     jq -r -S -c .integration.matrix[] "${ROOTDIR}/config.json" \
       && printf "\0"
   )
 
-  util::tools::ginkgo::install --directory "${ROOTDIR}/.bin"
   util::tools::buildpack-packager::install --directory "${ROOTDIR}/.bin"
   util::tools::cf::install --directory "${ROOTDIR}/.bin"
 
@@ -32,19 +79,23 @@ function main() {
 
     echo "Running integration suite (cached: ${cached}, parallel: ${parallel})"
 
-    specs::run "${harness}" "${cached}" "${parallel}"
+    specs::run "${cached}" "${parallel}" "${stack}" "${platform}" "${token:-}"
   done
 }
 
 function specs::run() {
-  local harness cached parallel
-  harness="${1}"
-  cached="${2}"
-  parallel="${3}"
+  local cached parallel stack platform token
+  cached="${1}"
+  parallel="${2}"
+  stack="${3}"
+  platform="${4}"
+  token="${5}"
 
   local nodes cached_flag serial_flag
   cached_flag="--cached=${cached}"
-  serial_flag="-serial=true"
+  serial_flag="--serial=true"
+  platform_flag="--platform=${platform}"
+  token_flag="--github-token=${token}"
   nodes=1
 
   if [[ "${parallel}" == "true" ]]; then
@@ -53,23 +104,9 @@ function specs::run() {
   fi
 
   local buildpack_file
-  buildpack_file="$(buildpack::package "1.2.3" "${cached}")"
+  buildpack_file="$(buildpack::package "1.2.3" "${cached}" "${stack}")"
 
-  if [[ "${harness}" == "gotest" ]]; then
-    specs::gotest::run "${nodes}" "${cached_flag}" "${serial_flag}" "${buildpack_file}"
-  else
-    specs::ginkgo::run "${nodes}" "${cached_flag}" "${serial_flag}" "${buildpack_file}"
-  fi
-}
-
-function specs::gotest::run() {
-  local nodes cached_flag serial_flag buildpack_file
-  nodes="${1}"
-  cached_flag="${2}"
-  serial_flag="${3}"
-  buildpack_file="${4}"
-
-  CF_STACK="${CF_STACK:-"${stack}"}" \
+  CF_STACK="${stack}" \
   BUILDPACK_FILE="${BUILDPACK_FILE:-"${buildpack_file}"}" \
   GOMAXPROCS="${GOMAXPROCS:-"${nodes}"}" \
     go test \
@@ -79,32 +116,16 @@ function specs::gotest::run() {
       -v \
         "${src}/integration" \
          "${cached_flag}" \
+         "${platform_flag}" \
+         "${token_flag}" \
          "${serial_flag}"
 }
 
-function specs::ginkgo::run(){
-  local nodes cached_flag serial_flag buildpack_file
-  nodes="${1}"
-  cached_flag="${2}"
-  serial_flag="${3}"
-  buildpack_file="${4}"
-
-  CF_STACK="${CF_STACK:-"${stack}"}" \
-  BUILDPACK_FILE="${BUILDPACK_FILE:-"${buildpack_file}"}" \
-    ginkgo \
-      -r \
-      -mod vendor \
-      --flakeAttempts "${GINKGO_ATTEMPTS:-2}" \
-      -nodes "${nodes}" \
-      --slowSpecThreshold 60 \
-        "${src}/integration" \
-      -- "${cached_flag}" "${serial_flag}"
-}
-
 function buildpack::package() {
-  local version cached
+  local version cached stack
   version="${1}"
   cached="${2}"
+  stack="${3}"
 
   local name cached_flag
   name="buildpack-v${version}-uncached.zip"
@@ -120,6 +141,7 @@ function buildpack::package() {
   bash "${ROOTDIR}/scripts/package.sh" \
     --version "${version}" \
     --output "${output}" \
+    --stack "${stack}" \
     "${cached_flag}" > /dev/null
 
   printf "%s" "${output}"
