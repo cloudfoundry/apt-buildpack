d8e0a624fcef7f10bc2f709c7fff32b6a25d636d
ramonskie <ramonmakkelie@gmail.com>
2026-03-02 16:30:57 +0100

    migrate to ginkgo v2
    Upgrade github.com/onsi/ginkgo v1.16.5 -> v2 and update all import
    paths from github.com/onsi/ginkgo to github.com/onsi/ginkgo/v2.

--- src/apt/apt/apt_suite_test.go
+++ src/apt/apt/apt_suite_test.go
@@ -1,7 +1,7 @@
 package apt_test
 
 import (
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"testing"
--- src/apt/brats/brats_test.go
+++ src/apt/brats/brats_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/cloudfoundry/libbuildpack/cutlass"
 
 	"github.com/cloudfoundry/libbuildpack/bratshelper"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
